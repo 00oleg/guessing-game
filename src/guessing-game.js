483636c88851466daae21ebf1bb81f0cb1ffd24a
Oleg Bendiucov <punisher8585@gmail.com>
2023-10-01 12:10:11 +0300

    feat: resolve Guessing Game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.avg = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.avg = max;
     }
 
     guess() {
-
+        this.avg = Math.ceil((this.max + this.min) / 2);
+        return this.avg;
     }
 
     lower() {
-
+        this.max = this.avg;
     }
 
     greater() {
-
+        this.min = this.avg;
     }
 }
 
